adc46c8b4b0af4df86168cdc10bb9d7a648116a7
Javiux <92271705+Javiux@users.noreply.github.com>
2022-05-07 22:07:14 -0500

    commit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>Hola Planeta</title>
  </head>
  <body>
- <?php echo '<p>Hola Mundo</p>'; ?> 
+ <?php echo '<p>Hola Planeta :)</p>'; ?> 
  </body>
 </html>
